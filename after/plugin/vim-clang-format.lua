e525b6da01e713d1f9ef42fdb18e6179e277cd50
Robin Elrharbi-Fleury <robin.elrharbi-fleury@gmail.com>
2023-06-20 08:34:26 +0100

    Clang format

---
+++ after/plugin/vim-clang-format.lua
@@ -0,0 +1,2 @@
+vim.keymap.set("n", "<leader>cf", "<cmd>ClangFormat<CR>")
+vim.keymap.set("v", "<leader>cf", "<cmd>ClangFormat<CR>")
--- lua/robin/packer.lua
+++ lua/robin/packer.lua
@@ -6,7 +6,10 @@ vim.cmd.packadd('packer.nvim')
 return require('packer').startup(function(use)
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
+
+  -- Robin's favourite
   use 'tpope/vim-commentary'
+  use 'rhysd/vim-clang-format'
 
   use {
 	  'nvim-telescope/telescope.nvim', tag = '0.1.0',
